102fa975ebc781dbcbf3b39379ee258f5ded463f
mathlon <92529978+mathlon26@users.noreply.github.com>
2025-02-17 15:28:00 +0100

    Initial popup

--- js/popup.js
+++ js/popup.js
@@ -4,3 +4,16 @@
 javascript for the extension popup
 
 */
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.storage.local.get("studentData", (result) => {
+        if (result.studentData) {
+            document.getElementById("output").textContent = JSON.stringify(result.studentData, null, 2);
+        } else {
+            document.getElementById("output").textContent = "No data found.";
+        }
+
+        console.log(result.studentData);
+    });
+});
