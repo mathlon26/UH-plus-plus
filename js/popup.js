3c402afe79b5ba8c77976aa21137e4b34af116c7
mathlon <92529978+mathlon26@users.noreply.github.com>
2025-02-17 15:48:05 +0100

    Added  name in  popup and placeholder settings page

--- js/popup.js
+++ js/popup.js
@@ -6,6 +6,11 @@ javascript for the extension popup
 */
 
 
+const displayName = (voornaam, achternaam) => {
+    document.getElementById("usernameDisplay").innerText = `${voornaam} ${achternaam}`;
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.get("studentData", (result) => {
         if (result.studentData) {
@@ -14,6 +19,16 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("output").textContent = "No data found.";
         }
 
-        console.log(result.studentData);
+
+        const studentData = result.studentData;
+
+        displayName(studentData.TopVoornaam, studentData.TopNaam);
+
     });
+});
+
+
+document.getElementById("settingsButton").addEventListener("click", () => {
+    const settingsUrl = chrome.runtime.getURL("html/settings.html");
+    window.open(settingsUrl, "_blank");  // Open the settings page in a new tab
 });
--- js/settings.js
+++ js/settings.js
@@ -4,3 +4,5 @@
 javascript for the settings.html page
 
 */
+
+// TODO implement settings, grab user and settings from storage and initialize settings on load
