2047effb6b9968f524ea73568eb8b60dd963eddc
mathlon <92529978+mathlon26@users.noreply.github.com>
2025-02-19 20:26:02 +0100

    update  frontend

--- chrome/src/JS/background.js
+++ chrome/src/JS/background.js
@@ -1,7 +1,23 @@
 // background.js
 
+let currentTab = "home";
+
+
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    async function getContentPage(pageName) {
+        console.log("GET content " + pageName);
+        let url = `src/pages/content/${pageName}.html`;
+        await fetch(chrome.runtime.getURL(url))
+            .then(response => response.text())
+            .then(html => sendResponse({ html: html}))
+            .catch(err => sendResponse({ error: err }));
+
+        return false;
+    }
+
+
     if (message.action == "GET::custom_html") {
         console.log("test");
         var url = message.lang == "04" ? "src/pages/custom_en.html" : "src/pages/custom_nl.html";
@@ -12,5 +28,21 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             .catch(err => sendResponse({ error: err }));
         return true;
     }
+    else if (message.action == "POST::updateCurrentTab") {
+        currentTab = message.data;
+        console.log("POST set ", currentTab);
+    }
+    else if (message.action == "GET::currentTab"){
+        console.log("GET get ", currentTab);
+        sendResponse({ currentTab: currentTab });
+        return false;
+    }
+    else if (message.action == "GET::contentPage") {
+        getContentPage(message.page);
+        return false;
+    }
+
+
+
     return false;
 });
